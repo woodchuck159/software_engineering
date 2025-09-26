786d186c8f88cc8eae810d74462b480ea1483dff
Alec Andrulis <81585258+alecandrulis@users.noreply.github.com>
2025-09-26 15:53:45 -0400

    returned wrong value on error

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def main() -> int:
 
 
     if (verbosity == None or log_file_location == None or gen_ai_key == None or github_token == None):
-        return -1
+        return 1
 
 
     logfile = "LOG_FILE.txt"
