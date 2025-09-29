c2535420326f41e774d2fedfe053f3becb58d38e
Andrew Kloosterman <aklooste@purdue.edu>
2025-09-28 20:48:12 -0400

    Updated get liscnse

--- get_model_metrics.py
+++ get_model_metrics.py
@@ -18,7 +18,24 @@ def get_model_README(namespace: str, repo: str, rev: str = "main") -> str:
 
     return ReadME_filepath
 
+def get_model_README(namespace: str, repo: str, rev: str = "main") -> str:
+    api = HuggingFaceApi(namespace, repo, rev)
+                     
+    ReadME_filepath = api.download_file("model_file_download", "README.md")
 
+    return ReadME_filepath
+
+
+def get_model_license(namespace: str, repo: str, rev: str = "main") -> str:
+    api = HuggingFaceApi(namespace, repo, rev)
+
+    tags = api.get_model_info().get("tags", [])
+
+    for t in tags:
+        if t.startswith("license:"):
+            return t.split("license:")[-1]                 
+    return ""
 
 if __name__ == "__main__":
-    metrics = get_model_README("openai-community", "gpt2")
+    metrics = get_model_license("openai-community", "gpt2")
+    print(metrics)
